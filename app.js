b3d163b0e78e209a92bc3a08b79438f936360edd
JhoanaRG <jhoanasarayrodriguezgonzalez@gmail.com>
2024-02-01 12:46:07 -0400

    face 4 terminada ,jhoana

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
+//-*-coding:utf-8 -*-
 //Librerias y dependencias
-require('dotenv').config();
-const http = require('http');
-const express = require('express');
-const app = express();
+const cookieParser = require('cookie-parser');
 const path = require('path');
-const baseDatos = require('./models/base.js');
-const {contrasena,user} = process.env;
+require('dotenv').config();
+var handlebars = require('express-handlebars')
+const Recaptcha = require('express-recaptcha').RecaptchaV2;
+const recaptcha = new Recaptcha('6LcCaz8pAAAAAFIACou2yj64ZXxTKEFRKuT7mHzZ', '6LcCaz8pAAAAAH_qt_SnKaXzMvBoTnUEoG_MjiWn');
+const multer = require('multer');
+const http = require('http');
+const { obtenerDireccionIP} = require('./utils/direccionIP.js');
+const express = require('express');
+
+const app = express();
+
+//Configuracion socket.io para las notificaciones realTime
+
+const server = http.createServer(app);
+
+const {Server} = require('socket.io');
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+
+console.log('Un usuario se a conectado');
+
+socket.emit('mensajeServer', '¡Hola, cliente!');
+
+socket.on('disconnect',()=>{
+
+console.log('un usuario se a desconectado');
+
+});
+
+});
+
+//recursos que se van a cargar en el server 
 app.use(express.static(__dirname+'/static'));
-//-----------------------------------------------------------------
+
 //Configuración del Servidor
 app.set('view engine','ejs');//definimos el motor de plantilla con archivos ejs
 app.set('views',path.join(__dirname,"./views"));//definimos la ruta del motor de plantilla
 app.use(express.urlencoded({extended:false}));//permite recuperar los valores publicados en un request
-port = app.listen(5000);
-console.log('Servidor corriendo exitosamente en el puerto 5000');
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(obtenerDireccionIP);
+const jwt = require('jsonwebtoken');
+const bodyParser= require('body-parser');
+//app.use(bodyParser.urlencoded({extended: true}));
+
+const baseDatos = require('./models/base.js');
+const utils = require('./utils/uploadImg.js');
+//middleware para verificar cliente
+const {verifyToken} = require('./utils/JWT.js');
+//middleware para verificar admin
+const {verifyToken2} = require('./utils/JWT2.js');
+//Variables de entorno
+const {contrasena,user,port,secretKey2} = process.env;
+let ext;
+//--------------------------------------------------------------
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './static/uploads')
+  },
+  filename: function (req, file, cb) {
+    ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + utils.getContentType(ext))
+  }
+})
+
+let upload = multer({ storage: storage });
+//---------------------------------------------------------------
 
 
 //-----------------------------------------------------------
@@ -24,7 +81,14 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/login',(req,res)=>{
-res.render('iniciarSesion.ejs');
+res.render('login.ejs',{
+   og: {
+      title: 'Juegos de mesa',
+      description: 'Venta de juegos de mesa',
+      image: 'https://images.pexels.com/photos/1796794/pexels-photo-1796794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      // Otros metadatos OGP que desees especificar
+      }
+});
 });
 
 
@@ -32,10 +96,18 @@ app.post('/login',(req,res)=>{
 
  const {admin,password} = req.body;
 
+ const dato= {
+  admin,
+  password
+ }
+
    if(admin === user && password === contrasena){
+    const token = jwt.sign(dato,secretKey2,{expiresIn:60 * 60 * 24});
+   // Guardar token en cookies
+    res.cookie('token2', token, { httpOnly: true, secure: true });
     res.redirect('/productos');
    }else{
-    res.render('password.ejs');
+   res.json({ERROR:'Contraseña o usuario incorrecto vuelve a intentarlo'});
    }
 
 });
@@ -46,12 +118,12 @@ res.render('add.ejs');
 });
 
 //---------------------------------------------------------
-app.get('/addImagen',(req,res)=>{
-res.render('addImagen.ejs');
+app.get('/addImagen/:id',(req,res)=>{
+baseDatos.getImagen(req,res);
 });
 
 
-app.post('/addImagen',(req,res)=>{
+app.post('/addImagen/:id',upload.single('img'),(req,res)=>{ 
 baseDatos.aggIMG(req,res);
 });
 
@@ -61,7 +133,7 @@ baseDatos.aggDato(req,res);
 });
 
 
-app.get('/productos',(req,res)=>{
+app.get('/productos',verifyToken2,(req,res)=>{
   baseDatos.mostrarProductos(req,res);
 });
 //-------------------------------------------------------
@@ -86,7 +158,7 @@ app.post('/delete/:id', (req, res) => {
  baseDatos.deletee(req,res);
 });
 //------------------------------------------------------
-app.get('/categorias', (req, res) => {
+app.get('/categorias',verifyToken2,(req, res) => {
  baseDatos.getCategorias(req,res);
 });
 //-------------------------------------------------------
@@ -106,8 +178,191 @@ app.post('/updateCategoria/:id',(req,res)=>{
 baseDatos.updateCateg(req,res);
 });
 //-------------------------------------------------------
-//Metodo para manejar rutas no encontradas
-app.get('/*',(req,res)=>{
-res.render('notfound.ejs')
+app.get('/eliminarCategoria/:id',(req,res)=>{
+baseDatos.deleteCategoriaGET(req,res);
+})
+//-------------------------------------------------------
+app.get('/clientes',verifyToken,(req,res)=>{
+  console.log('mostrando pagina la cliente!');
+baseDatos.ClientesGET(req,res);
+})
+//-------------------------------------------------------
+app.post('/cliente', (req, res) => {
+ baseDatos.filtrar(req,res);
 });
 //-------------------------------------------------------
+app.get('/clientico',(req, res) => {
+ baseDatos.filtrar2(req,res);
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',(req,res)=>{
+baseDatos.detalles(req,res);
+});
+//-------------------------------------------------------
+app.get('/ruta',(req, res) => {
+  const {nombre,codigo,precio,descripcion,calidad,cantidad,url} = req.query;
+
+  let datos = {
+    nombre:nombre,
+    codigo:codigo,
+    precio:precio,
+    descripcion:descripcion,
+    calidad:calidad,
+    cantidad:cantidad,
+    url:url
+  }
+
+  console.log(datos,'Valor de Busqueda--por fin');
+  res.render('buscar.ejs',{result:datos});
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',(req,res)=>{
+baseDatos.detalles(req,res);
+});
+//-------------------------------------------------------
+app.get('/loginUsers',(req,res)=>{
+baseDatos.loginUsers(req,res);
+});
+//------------------------------------------------------
+app.post('/loginUsers',(req,res)=>{
+  baseDatos.postLoginCliente(req,res);
+})
+//------------------------------------------------------
+app.get('/registroUsers',(req,res)=>{
+  baseDatos.registroUsers(req,res);
+});
+//------------------------------------------------------
+app.post('/registroUsuariosPost',recaptcha.middleware.verify,(req,res)=>{
+
+
+   if(!req.recaptcha.error){
+    // El reCAPTCHA se ha verificado correctamente
+     baseDatos.registroUsuariosPost(req,res); 
+  } else{
+    // El reCAPTCHA no se ha verificado correctamente
+    res.send('Debes validar el Recaptcha');
+  } 
+})
+//------------------------------------------------------
+app.get('/comprar/:id',obtenerDireccionIP,verifyToken,(req,res)=>{
+  res.clearCookie('transaccion');
+  baseDatos.comprar(req,res);
+});
+//------------------------------------------------------
+app.post('/comprarPost',verifyToken,async (req,res)=>{
+baseDatos.comprarPOST(req,res);
+})
+//------------------------------------------------------
+app.get('/transaction',(req,res)=>{
+const transaction = req.cookies.transaccion;
+console.log('transaction desde controllers',transaction);
+ res.json({transaction}); 
+});
+//------------------------------------------------------
+app.get('/eliminarTransaction',(req,res)=>{
+ res.clearCookie('transaccion');
+ res.json({message:'transaccion eliminada'});
+})
+//------------------------------------------------------
+app.get('/usuarios',verifyToken2,(req,res)=>{
+baseDatos.mostrarUsers(req,res);
+})
+//------------------------------------------------------
+//------------------------------------------------------
+app.get('/compras',verifyToken2,(req,res)=>{
+baseDatos.MostrarCompras(req,res);
+})
+
+//------------------------------------------------------
+app.get('/addUser',verifyToken2,(req,res)=>{
+baseDatos.addUsers(req,res);
+})
+//------------------------------------------------------
+app.post('/addUser',(req,res)=>{
+  baseDatos.addUsersPost(req,res);
+})
+//------------------------------------------------------
+app.get('/updateUser/:id',verifyToken2,(req,res)=>{
+baseDatos.updateUser(req,res);
+})
+//------------------------------------------------------
+app.post('/updateUser/:id',(req,res)=>{
+baseDatos.updateUserPost(req,res);
+})
+//------------------------------------------------------
+app.get('/deleteUser/:id',(req,res)=>{
+baseDatos.deleteUser(req,res);
+});
+//------------------------------------------------------
+app.get('/deleteCompra/:id',(req,res)=>{
+baseDatos.deleteCompra(req,res);
+})
+//////////////////////////////////////////////////
+app.post('/puntuaciones',verifyToken,(req,res)=>{
+baseDatos.puntuaciones(req,res);
+});
+
+app.get('/recuperarPassword',(req,res)=>{
+res.render('recuperarPassword.ejs',{
+    og: {
+      title: 'Juegos de mesa',
+      description: 'Venta de juegos de mesa',
+      image: 'https://images.pexels.com/photos/1796794/pexels-photo-1796794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      // Otros metadatos OGP que desees especificar
+      }
+});
+});
+
+//------------------------------------------------------
+app.post('/recuperarPassword',(req,res)=>{
+baseDatos.enviarEmailRecuperacion(req,res);
+});
+//------------------------------------------------------
+app.get('/restablecer-contrasena',(req,res)=>{  
+
+const token = req.query.token;
+const UserName = req.query.userName;
+console.log(userName);
+
+const tokenCookies = req.cookies.securityToken;
+
+console.log(tokenCookies,'tokenCookies');
+console.log(token,'token');
+
+
+res.render('restablecer.ejs',{user:UserName,  og: {
+      title: 'Juegos de mesa',
+      description: 'Venta de juegos de mesa',
+      image: 'https://images.pexels.com/photos/1796794/pexels-photo-1796794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      // Otros metadatos OGP que desees especificar
+      }});
+
+});
+//------------------------------------------------------
+app.post('/restablecer-contrasena',(req,res)=>{
+
+baseDatos.restablecerPost(req,res);
+
+});
+//------------------------------------------------------
+//logout cliente
+app.get('/logout',(req, res) => {
+  //metodo para borrar la cookie
+  res.clearCookie('token');
+  res.redirect('/');
+});
+//------------------------------------------------------
+//logout administrador
+app.get('/logout2',(req, res) => {
+  res.clearCookie('token2');
+  res.redirect('/');
+});
+//-------------------------------------------------------
+//Metodo para manejar rutas no encontradas
+app.get('/*',(req,res)=>{
+res.render('notfound.ejs');
+});
+//-------------------------------------------------------
+server.listen(port,()=>{
+  console.log(`Servidor corriendo exitosamente en el puerto ${port}`);
+});
